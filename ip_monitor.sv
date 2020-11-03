1eb3d2aba962d727122c7c9080e9e2a3a2d05b0a
Rishav Kumar <kumarrishav14@gmail.com>
2020-11-03 23:59:49 +0530

    Updated code for better readability

--- ip_monitor.sv
+++ ip_monitor.sv
@@ -1,20 +1,34 @@
-class ip_monitor;
-    virtual APB_intf.IPMON ipmon_intf;
+/*  Input monitor will make the transaction packet from the sampled APB signals (MASTER -> SLAVE).
+    As some packets may have mutiple transfer cycle in one packet, packet construction is bit complicated
+    Whenever driver completes transfer of one packet it triggers drv_done event which is used in monitor 
+    as a signal that one packet has completed and send it to scoreboard. */
 
+class ip_monitor;
+    // Interface and communication variables
+    virtual APB_intf.IPMON ipmon_intf;
+    virtual APB_intf intf;
     mailbox #(transaction) ipmon2opmon;
     event drv_done;
+
     transaction trans, trans2opmon;
 
+    // Helper variables
     int i;
     bit sampled;
-    function new(virtual APB_intf.IPMON ipmon_intf, mailbox #(transaction) ipmon2opmon, event drv_done);
+
+    function new(virtual APB_intf.IPMON ipmon_intf, virtual APB_intf intf, mailbox #(transaction) ipmon2opmon, event drv_done);
         this.ipmon_intf = ipmon_intf;
+        this.intf = intf;
         this.ipmon2opmon = ipmon2opmon;
         this.drv_done = drv_done;
 
         trans = new;
     endfunction //new()
 
+    /*  monitor task will sample the signals only when the PENABLE is HIGH (all signals are stable). Also
+        as the PENABLE can be HIGH for more than 1 cycle due to wait cycles, the data is sampled only once
+        per PENABLE high. 
+        NOTE: $rose() does not work. To make $rose() work, implementation is complicated*/
     task monitor();
         @(ipmon_intf.ipmon_cb);
         if(ipmon_intf.ipmon_cb.PENABLE == 1 && !sampled) begin
@@ -30,6 +44,8 @@ class ip_monitor;
         if(ipmon_intf.ipmon_cb.PENABLE == 0) sampled = 0;                  
     endtask
 
+    // Starts the monitor. sends the packet to the Scoreboard only when driver completes
+    // the transfer (indicated by drv_done)
     task start();
         fork
             i = 0;
