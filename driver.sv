2ccd634ff55f18a0dd11bf0d1024191fb5e194dc
Rishav Kumar <kumarrishav14@gmail.com>
2020-11-03 13:44:50 +0530

    Updated the drive sequence

--- driver.sv
+++ driver.sv
@@ -13,4 +13,39 @@ class driver;
 
         trans = new;
     endfunction //new()
+
+    task drive();
+        @(drv_intf.drv_cb);
+        drv_intf.drv_cb.PSEL1   <= trans.PSEL1;
+        drv_intf.drv_cb.PWRITE  <= trans.PWRITE;
+        drv_intf.drv_cb.PWDATA  <= trans.PWDATA;
+        drv_intf.drv_cb.PADDR   <= trans.PADDR;
+        drv_intf.drv_cb.PRESETn <= trans.PRESETn;
+        for(int i=1; i<trans.PADDR.size(); i++) begin
+            wait(PREADY == 1);
+            drv_intf.drv_cb.PWRITE  <= trans.PWRITE;
+            drv_intf.drv_cb.PWDATA  <= trans.PWDATA;
+            drv_intf.drv_cb.PADDR   <= trans.PADDR;
+            drv_intf.drv_cb.PRESETn <= trans.PRESETn;
+        end
+        @(drv_intf.drv_cb);
+        
+        drv_intf.drv_cb.PWRITE  <= trans.PWRITE;
+        drv_intf.drv_cb.PWDATA  <= trans.PWDATA;
+        drv_intf.drv_cb.PADDR   <= trans.PADDR;
+        drv_intf.drv_cb.PRESETn <= trans.PRESETn;
+        @(drv_intf.drv_cb);
+        drv_intf.drv_cb.ENABLE  <= trans.PENABLE;
+    endtask
+
+    task start();
+        fork
+            forever begin
+                gen2drv.get(trans);
+                trans.printf("FROM GENERATOR");
+                drive();
+                ->drv_done;
+            end
+        join_none
+    endtask
 endclass //driver
